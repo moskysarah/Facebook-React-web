e3df731e047ff91179c6fd5dbafc160ea7e8a46e
sarahmosky <sarahmosky@gmail.com>
2026-01-19 15:38:55 +0100

    add a screenshort on readme

--- src/components/marketplaceSection.tsx
+++ src/components/marketplaceSection.tsx
@@ -4,7 +4,7 @@ function MarketPlace() {
     return (
         
         
-        <div className="grid grid-cols-3 gap-2 shadow-lg p-4  rounded-lg   justify-center">
+        <div className="grid grid-cols-4 gap-2 shadow-lg p-4  rounded-lg   justify-center">
             
             <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[27rem] border-1 border-gray-500">
                 <img src="/profil/9a14c86e49dcb14c37584a9cb3d50187.jpg" alt=""
@@ -52,6 +52,25 @@ function MarketPlace() {
                 
             </div>
              <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[27rem] border-1 border-gray-500">
+                <img src="/profil/d96b8ef5579e88184165058fa1f59a4b.jpg" alt=""
+                   className="rounded-t-lg w-full h-75 object-cover "
+                />
+                <h3 className="text-center mt-3 font-semibold text-white">95 .000 FC</h3>
+                <p className="text-center text-gray-600">Sac Sport</p>
+                <p className="text-center text-gray-400">Kinshasa </p>
+                
+            </div>
+            
+            <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[27rem] border-1 border-gray-500">
+                <img src="/profil/d96b8ef5579e88184165058fa1f59a4b.jpg" alt=""
+                   className="rounded-t-lg w-full h-75 object-cover "
+                />
+                <h3 className="text-center mt-3 font-semibold text-white">95 .000 FC</h3>
+                <p className="text-center text-gray-600">Sac Sport</p>
+                <p className="text-center text-gray-400">Kinshasa </p>
+                
+            </div>
+            <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[27rem] border-1 border-gray-500">
                 <img src="/profil/d96b8ef5579e88184165058fa1f59a4b.jpg" alt=""
                    className="rounded-t-lg w-full h-75 object-cover "
                 />
