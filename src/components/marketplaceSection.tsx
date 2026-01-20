ba6b4e5b400b63d58ba32b5376497c99e064e058
sarahmosky <sarahmosky@gmail.com>
2026-01-21 00:06:35 +0100

    remove some errors

--- src/components/marketplaceSection.tsx
+++ src/components/marketplaceSection.tsx
@@ -6,54 +6,54 @@ function MarketPlace() {
         
         <div className="grid grid-cols-4 gap-2 shadow-lg p-4  rounded-lg   justify-center">
             
-            <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[27rem] border-1 border-gray-500">
+            <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[24rem] border-1 border-gray-500">
                 <img src="/profil/9a14c86e49dcb14c37584a9cb3d50187.jpg" alt=""
-                   className="rounded-t-lg w-full h-75 object-cover "
+                   className="rounded-t-lg w-full h-60 object-cover "
                 />
                 <h3 className="text-center mt-3 font-semibold text-white">15$</h3>
                 <p className="text-center text-gray-600">Gourde</p>
                 <p className="text-center text-gray-400">Kinshasa </p>
                 
             </div>
-            <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[27rem] border-1 border-gray-500">
+            <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[24rem] border-1 border-gray-500">
                    <img src="/profil/2c2038d84d72fa33092d244dd2fbddde.jpg" alt=""
-                      className="rounded-t-lg w-full h-75 object-cover "
+                      className="rounded-t-lg w-full h-60 object-cover "
                     />
                 <h3 className="text-center mt-3 font-semibold text-white">50$</h3>
                 <p className="text-center text-gray-600">Sac Nike</p>
                 <p className="text-center text-gray-400">Kinshasa </p>
                 
             </div>
-             <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[27rem] border-1 border-gray-500">
+             <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[24rem] border-1 border-gray-500">
                 <img src="/profil/3c7192af08279bff2da4a6b4aef99047.jpg" alt=""
-                   className="rounded-t-lg w-full h-75 object-cover "
+                   className="rounded-t-lg w-full h-60 object-cover "
                 />
                 <h3 className="text-center mt-3 font-semibold text-white">40$</h3>
                 <p className="text-center text-gray-600">Jogging</p>
                 <p className="text-center text-gray-400">Kinshasa </p>
                 
             </div>
-             <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[27rem] border-1 border-gray-500">
+             <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[24rem] border-1 border-gray-500">
                 <img src="/profil/6f1e5e0ec01d3702d63dc996ebaa4271.jpg" alt=""
-                   className="rounded-t-lg w-full h-75 object-cover "
+                   className="rounded-t-lg w-full h-60 object-cover "
                 />
                 <h3 className="text-center mt-3 font-semibold text-white">25$</h3>
                 <p className="text-center text-gray-600">Chapeau Nike </p>
                 <p className="text-center text-gray-400">Kinshasa </p>
                 
             </div>
-             <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[27rem] border-1 border-gray-500">
+             <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[24rem] border-1 border-gray-500">
                 <img src="/profil/dc3c9070b5d3802d3b12d0b7d4262cdf.jpg" alt=""
-                   className="rounded-t-lg w-full h-75 object-cover "
+                   className="rounded-t-lg w-full h-60 object-cover "
                 />
                 <h3 className="text-center mt-3 font-semibold text-white">25$</h3>
                 <p className="text-center text-gray-600">Chapeau Nike</p>
                 <p className="text-center text-gray-400">Kinshasa </p>
                 
             </div>
-             <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[27rem] border-1 border-gray-500">
+             <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[24rem] border-1 border-gray-500">
                 <img src="/profil/d96b8ef5579e88184165058fa1f59a4b.jpg" alt=""
-                   className="rounded-t-lg w-full h-75 object-cover "
+                   className="rounded-t-lg w-full h-60 object-cover "
                 />
                 <h3 className="text-center mt-3 font-semibold text-white">80$</h3>
                 <p className="text-center text-gray-600">Sac Nike</p>
@@ -61,18 +61,18 @@ function MarketPlace() {
                 
             </div>
             
-            <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[27rem] border-1 border-gray-500">
+            <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[24rem] border-1 border-gray-500">
                 <img src="https://i.pinimg.com/1200x/38/6c/13/386c13acf3f9a5030e63511a2aca00be.jpg" alt=""
-                   className="rounded-t-lg w-full h-75 object-cover "
+                   className="rounded-t-lg w-full h-60 object-cover "
                 />
                 <h3 className="text-center mt-3 font-semibold text-white">50$</h3>
                 <p className="text-center text-gray-600">Sac Puma</p>
                 <p className="text-center text-gray-400">Kinshasa </p>
                 
             </div>
-            <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[27rem] border-1 border-gray-500">
+            <div className="shadow-lg p-4 bg-[#242526] rounded-lg w-full max-w-65 h-[24rem] border-1 border-gray-500">
                 <img src="/profil/d96b8ef5579e88184165058fa1f59a4b.jpg" alt=""
-                   className="rounded-t-lg w-full h-75 object-cover "
+                   className="rounded-t-lg w-full h-60 object-cover "
                 />
                 <h3 className="text-center mt-3 font-semibold text-white">45$</h3>
                 <p className="text-center text-gray-600">Sac Nike</p>
